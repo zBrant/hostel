63102ee35eeee972e30bfd7e3463d8676ad12475
EduardoBrant <carlos23brant@hotmail.com>
2024-11-15 19:30:35 -0400

    [COMMIT] - First commit

---
+++ hostel-api/src/main/java/io/github/zbrant/hostelapi/HostelApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.zbrant.hostelapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostelApiApplication.class, args);
+	}
+
+}
---
+++ hostel-api/src/test/java/io/github/zbrant/hostelapi/HostelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.zbrant.hostelapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
